387b348a489a85f82d8d70c48356ad84be8220ed
simal24k <simal.mpgss@gmail.com>
2025-09-18 22:17:46 +0500

    Update Q1.cpp

--- Lab 04/post lab/Q1.cpp
+++ Lab 04/post lab/Q1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+void Linear(int arr[], int x, int n){
+    bool flag=false;
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            cout<<"Found at index "<<i+1<<endl;
+            flag=true;
+        }
+    }
+    if(!flag){
+        cout<<"Not found"<<endl;
+    }
+}
+
+int main(){
+    int sz;
+    cout<<"Size of array: ";
+    cin>>sz;
+
+    int *data=new int[sz];
+    for(int i=0;i<sz;i++){
+        cout<<"Enter element "<<i+1<<": ";
+        cin>>data[i];
+    }
+
+    int val;
+    cout<<"Enter value to search: ";
+    cin>>val;
+
+    Linear(data,val,sz);
+
+    delete[] data;
+    return 0;
+}
