af4990dc1b24b8cca50c9b842f45b425eb198e3d
simal24k <simal.mpgss@gmail.com>
2025-09-18 22:26:40 +0500

    Update Q3.cpp

--- Lab 04/post lab/Q3.cpp
+++ Lab 04/post lab/Q3.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
 
+void selection(int arr[], int n){
+    for(int i=0;i<n;i++){
+        int index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[index]){
+                index=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[index];
+        arr[index]=temp;
+    }
+}
+
+int main(){
+    int sz;
+    cout<<"Enter size of array: ";
+    cin>>sz;
+
+    int *nums = new int[sz];
+    for(int i=0;i<sz;i++){
+        cout<<"Value "<<i+1<<": ";
+        cin>>nums[i];
+    }
+
+    selection(nums,sz);
+
+    cout<<"Selection sorted array: ";
+    for(int i=0;i<sz;i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+
+    delete[] nums;
+    return 0;
+}
