f48b6443167034a2417673b38d077df8eceb69ae
simal24k <simal.mpgss@gmail.com>
2025-09-19 12:18:21 +0500

    Update Q3.cpp

--- Lab 05/Q3.cpp
+++ Lab 05/Q3.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int sumTail(int n, int total) {
+    if (n == 0) {
+        
+        return total;
+    }
+   
+    return sumTail(n - 1, total + n);
+}
+
+
+int sumNonTail(int n) {
+    if (n == 0) {
+        
+        return 0;
+    }
+   
+    return sumNonTail(n - 1) + n;
+}
+
+int main() {
+    int n = 7; 
+
+   
+    int tailResult = sumTail(n, 0);
+    cout << "Sum (Tail Recursion) from 1 to " << n << ": " << tailResult << endl;
+
+  
+    int nonTailResult = sumNonTail(n);  
+    cout << "Sum (Non-Tail Recursion) from 1 to " << n << ": " << nonTailResult << endl;
+
+    return 0;
+}
