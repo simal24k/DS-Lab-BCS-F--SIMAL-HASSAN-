14313fe3e504b2c1fa80dbafdfe9f0ad22539c43
simal24k <simal.mpgss@gmail.com>
2025-09-19 12:10:00 +0500

    Update Q2.cpp

--- Lab 05/Q2.cpp
+++ Lab 05/Q2.cpp
@@ -1,1 +1,45 @@
+#include<iostream>
+using namespace std;
 
+void printNumbers(int n) {
+    if (n < 1) {
+        
+        return;
+    }
+
+    cout << n << " \n ";
+
+    printNumbers(n - 1);
+}
+void functionB(int n);
+void functionA(int n){
+        if (n < 1) {
+        
+        return;
+    }
+
+    cout << n << " \n ";
+
+    functionB(n - 1);
+}
+void functionB(int n){        
+    if (n < 1) {
+        
+        return;
+    }
+
+    cout << n << " \n ";
+
+    functionA(n - 1);
+}
+int main() {
+    int nums;
+    cout<<"Enter number: "<<endl;
+    cin>>nums; 
+    printNumbers(nums);
+    cout<<"Function A: "<<endl;
+    functionA(nums);
+    cout<<"Function B: "<<endl;
+    functionB(nums);
+    return 0;
+}
