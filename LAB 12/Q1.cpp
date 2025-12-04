249f37bd74c59780f5379089a6f7fa89c99af54e
Simal Hassan <simal.mpgss@gmail.com>
2025-12-05 02:14:41 +0500

    Update Q1.cpp

--- LAB 12/Q1.cpp
+++ LAB 12/Q1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
 
+int main(){
+    string Text = "the quick brown fox jumps over the lazy dog";
+    string pattern = "the";
+
+    int n = Text.length();
+    int m = pattern.length();
+    int comparision = 0;
+    int result[200] ;
+    int count = 0;
+
+    for(int i = 0 ; i < n - m ; i++){
+        int j = 0 ;
+        while(j < m && Text[i+j] == pattern[j]){
+             comparision++ ;
+               j++;
+             if(j == m){
+                result[count] = i;
+                count++;
+             }
+        }
+    }
+
+    cout << "Output: [";
+    for(int i = 0 ; i < count ; i++){
+        cout << result[i] << " ";
+    }
+    cout << "]" << endl;
+    cout << "Comparisions: " << comparision;
+}
